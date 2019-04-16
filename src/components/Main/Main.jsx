e995c59ffcabfce13efd77d99b573b4cf82c0e79
Jason D'souza <jvincentdsouza@gmail.com>
2019-04-17 07:48:52 +1000

    added all information and styling

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,23 +7,124 @@ class MainBody extends React.Component {
   render() {
     return (
         <div className = "main-body">
-          <div className="info-card">
-            <h2 id="About">About Me</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel ex feugiat, pretium lectus et, imperdiet purus. Duis blandit magna ac nisl dignissim consectetur. Fusce ex purus, efficitur ac porttitor at, dictum eu tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus iaculis justo vitae iaculis convallis. Nam facilisis tortor nec libero suscipit volutpat. Curabitur non ultricies leo.
-
-Integer aliquet sagittis diam ac pharetra. Curabitur sem magna, venenatis imperdiet lorem quis, tempor semper ipsum. Vestibulum sit amet rhoncus ante, id pharetra tortor. Etiam condimentum varius tellus quis rhoncus. Phasellus eu ante vitae nibh pulvinar fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer ac quam euismod, dictum augue sit amet, tristique ipsum.</p>
+          <div className="info-card" id="About">
+            <h2>About Me</h2>
+              <p>
+                I'm a young, motivated and determined self-taught developer with skills 
+                in <span className="About-highlight">full-stack Javascript Web developement</span>,
+                a Coding Mentor in <span className="About-highlight">Python</span> for John Monash Science 
+                School and a Graduate of <span className="About-highlight">Civil Engineering (Honours) from Monash University</span>. 
+              </p>
+              <p>
+                I have worked in various areas, such as Retail, Geotechnical Engineering, and
+                Traffic Engineering. However I find my heart remains with computer science and 
+                software development.
+              </p>
+              <p>
+                My current ambition now inolves <span className="About-highlight">Web development</span> as I have grown wonder over the magic of the 
+                technologies related to the Internet. I have taught myself copious amounts of information from various resources
+                how to develop websites, and I have absolutely enjoyed every minute of it.
+              </p>
+                My current skillset consists of:
+                <div>
+                  <ul>
+                    <li>Javascript</li>
+                    <li>ES6</li>
+                    <li>ReactJs</li>
+                    <li>NodeJs and Express</li>
+                    <li>Databases such as MongoDB and PostgreSQL</li>
+                    <li>HTML5 and CSS3</li>
+                    <li>Python</li>
+                    <li>and many more libraries for Javascript and Python</li>
+                  </ul>
+                  <span>And planning to learn:</span>
+                  <ul>
+                    <li>GraphQL</li>
+                    <li>Redis</li>
+                    <li>Jest Testing</li>
+                  </ul>
+                </div>
+              <p>
+                I am also <span className="About-highlight">available for hire!</span>
+              </p>
           </div>
-          <div className="info-card">
-            <h2 id="Projects">Projects</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel ex feugiat, pretium lectus et, imperdiet purus. Duis blandit magna ac nisl dignissim consectetur. Fusce ex purus, efficitur ac porttitor at, dictum eu tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus iaculis justo vitae iaculis convallis. Nam facilisis tortor nec libero suscipit volutpat. Curabitur non ultricies leo.
 
-Integer aliquet sagittis diam ac pharetra. Curabitur sem magna, venenatis imperdiet lorem quis, tempor semper ipsum. Vestibulum sit amet rhoncus ante, id pharetra tortor. Etiam condimentum varius tellus quis rhoncus. Phasellus eu ante vitae nibh pulvinar fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer ac quam euismod, dictum augue sit amet, tristique ipsum.</p>
+          <div className="info-card" id="Projects">
+            <h2>Projects</h2>
+            <table>
+              <tbody>
+                <tr>
+                  <td className="Underline">
+                  <a 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    href="https://gentle-citadel-27836.herokuapp.com/">
+                      Personal Blog
+                  </a>
+                  </td>
+                  <td className="Project-info">
+                    <span className="Projects-highlight">Full stack</span> blog, featuring a front-end view and a back-end server
+                    alongside a <span className="Projects-highlight">CRUD app</span>. Technologies used involve <span className="Projects-highlight">MongoDB Atlas, 
+                    Express, ReactJs and NodeJs</span>.
+                  </td>
+                </tr>
+                <tr>
+                  <td className="Underline">
+                    <a 
+                      target="_blank" 
+                      rel="noopener noreferrer" 
+                      href="https://github.com/jvdsouza/weather-app">
+                        Weather App
+                    </a>
+                  </td>
+                  <td className="Project-info">
+                  <span className="Projects-highlight">Front-End</span> app utilising the <span className="Projects-highlight">Apixu API</span> to deliver weather temperature
+                    and weather status, alongside using <span className="Projects-highlight">Google Maps API</span> to show the location
+                    of where the weather is being recorded.
+                  </td>
+                </tr>
+                <tr>
+                  <td className="Underline">
+                    <a 
+                      target="_blank" 
+                      rel="noopener noreferrer" 
+                      href="https://www.fullstacktrends.com/">
+                      Full Stack Trends <br/>(Open Source Contribution)
+                    </a>
+                  </td>
+                  <td className="Project-info">
+                    An <span className="Projects-highlight">Open Source Project</span> for those who were part of one of the Udemy Courses
+                    I studied <a target="_blank" rel="noopener noreferrer" href="https://www.udemy.com/the-complete-web-developer-zero-to-mastery/">(Complete Web Developer 2019)</a>,
+                    highlighting the current trends of technologies in the Full-Stack Web Development world and their popularity.
+                    Responsible for <span className="Projects-highlight">building the modal</span> that lists the courses the students were partaking
+                    when contributing to the project.
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <div className="info-card">
-            <h2 id="Contact">Contact Me</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel ex feugiat, pretium lectus et, imperdiet purus. Duis blandit magna ac nisl dignissim consectetur. Fusce ex purus, efficitur ac porttitor at, dictum eu tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus iaculis justo vitae iaculis convallis. Nam facilisis tortor nec libero suscipit volutpat. Curabitur non ultricies leo.
 
-Integer aliquet sagittis diam ac pharetra. Curabitur sem magna, venenatis imperdiet lorem quis, tempor semper ipsum. Vestibulum sit amet rhoncus ante, id pharetra tortor. Etiam condimentum varius tellus quis rhoncus. Phasellus eu ante vitae nibh pulvinar fermentum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer ac quam euismod, dictum augue sit amet, tristique ipsum.</p>
+          <div className="info-card" id="Contact">
+            <h2>Contact Me</h2>
+            <p>You can get in touch with me through the following:</p>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Email</td>
+                  <td>jvincentdsouza@gmail.com</td>
+                </tr>
+                <tr>
+                  <td>Linkedin</td>
+                  <td>
+                    <a target="_blank" 
+                        rel="noopener noreferrer" 
+                        href="https://www.linkedin.com/in/jason-d-souza-861762100/">
+                          LinkedIn Profile
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
     );
