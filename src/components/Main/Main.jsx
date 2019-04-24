e472a6024d862b75a9158980054bda94419ce404
Jason D'souza <jvincentdsouza@gmail.com>
2019-04-25 09:40:38 +1000

    changed some styling and words

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,8 +11,7 @@ class MainBody extends React.Component {
             <h2>About Me</h2>
               <p>
                 I am a young, motivated and determined <span className="About-highlight">self-taught</span> developer with skills 
-                in <span className="About-highlight">full-stack Javascript Web developement</span>, and a Graduate of 
-                <span className="About-highlight"> Civil Engineering (Honours) from Monash University</span>. 
+                in <span className="About-highlight">full-stack Javascript Web developement</span>, and a Graduate of <span className="About-highlight">Civil Engineering (Honours) from Monash University</span>. 
               </p>
               <p>
                 Having worked in various areas I have gained numerous soft skills. These areas such as Retail, Geotechnical Engineering, and
@@ -21,12 +20,11 @@ class MainBody extends React.Component {
                 School and putting my best in helping <span className="About-highlight">teach young teenagers in year 10 and year 11 about computer science</span>.
               </p>
               <p>
-                I plan on continuing my self development in 
-                <span className="About-highlight"> computer science</span>, <span className="About-highlight">software engineering</span>,
+                I plan on continuing my self development in <span className="About-highlight">computer science</span>, <span className="About-highlight">software engineering</span>,
                 and <span className="About-highlight">web development</span>.
                 I am also looking for new opportunities to challenge myself and the knowledge I have procured
                 from the various courses and resources I have taken and read, further developing my extensive
-                knowledge, such as by taking on <a href="#Projects">projects</a> myself, in teams or open source projects.
+                knowledge, such as by taking on <a className="About-highlight" href="#Projects">projects</a> myself, in teams or open source projects.
               </p>
                 My current skillset consists of:
                 <div>
@@ -43,12 +41,15 @@ class MainBody extends React.Component {
                   <span>And planning to learn:</span>
                   <ul>
                     <li>GraphQL + Apollo + Prisma</li>
+                    <li>Agile development</li>
+                    <li>Amazon Web Services (AWS)</li>
                     <li>Redis</li>
                     <li>Jest Testing</li>
                     <li>CI/CD</li>
                     <li>Docker + Docker Compose</li>
                     <li>Rust + Web Assembly</li>
                     <li>Electron</li>
+                    <li>and open to anything else</li>
                   </ul>
                 </div>
               <p>
